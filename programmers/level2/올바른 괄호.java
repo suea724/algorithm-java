9f3108dbc977bdaef21f0f6837cd6da2f6eb32f0
suea724 <suea724@gmail.com>
2022-08-16 23:35:32 +0900

    20220816

--- programmers/level2/올바른 괄호.java
+++ programmers/level2/올바른 괄호.java
@@ -3,20 +3,17 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
 
-        String[] temp = s.split("");
-        Stack<String> stack = new Stack<>();
+        // split으로 String 배열 사용 시 효율성 0점 > charAt() 사용
+
+        Stack<Character> stack = new Stack<>();
         
-        // 닫는 괄호로 시작하거나 여는 괄호로 끝나면 false 반환
-        if (temp[0].equals(")") || temp[temp.length-1].equals("(")) {
-            return false;
-        }
-        
-        for (int i = 0 ; i < temp.length ; i ++) {
-            if (temp[i].equals("(")) { // 여는 괄호일 경우 stack에 push
-                stack.push(temp[i]);
+        for (int i = 0 ; i < s.length() ; i ++) {
+            
+            if (s.charAt(i) == '(') { // 여는 괄호일 경우 stack에 push
+                stack.push(s.charAt(i));
             } else { // 닫는 괄호일 경우 pop
                 
-                if (stack.isEmpty()) {
+                if (stack.isEmpty()) { // stack에 열린 괄호가 없으면 바로 false 반환
                     return false;
                 }
                 
